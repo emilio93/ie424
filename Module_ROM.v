0277e57705ba07d8de7df59290b6b5b101c2b6eb
Emilio Rojas <emilio93@gmail.com>
2018-05-16 20:18:52 -0600

    actualizar codigo de hola mundo

--- Module_ROM.v
+++ Module_ROM.v
@@ -3,8 +3,8 @@
 
 `define INICIO 8'd6
 `define LOOP1 8'd7
-`define LOOP2 8'd8
-`define LOOPINFINITO 8'd13
+`define LOOP2 8'd9
+`define LOOPINFINITO 8'd14
 
 module ROM
 (
@@ -17,22 +17,22 @@ begin
   0: oInstruction = {`NOP,24'd4000};
   1: oInstruction = {`STO,`R0,16'b0};
   2: oInstruction = {`STO,`R1,16'b1};
-  3: oInstruction = {`STO,`R13,16'b0};
-  4: oInstruction = {`STO,`R14,14'D1000};
-  5: oInstruction = {`STO,`R15,15'D1000};
+  3: oInstruction = {`STO,`R14,16'd100};
+  4: oInstruction = {`STO,`R15,16'd100};
+  5: oInstruction = {`STO,`R10,`H};
 // INICIO:
-  6: oInstruction = {`STO,`R2,`H};
+  6: oInstruction = {`STO,`R13,16'b0};
 // LOOP1:
   7: oInstruction = {`STO,`R12,16'b0};
+  8: oInstruction = {`NOP,24'd0};
 // LOOP2:
-  8: oInstruction = {`ADD,`R12,`R12,`R1};
-  9: oInstruction = {`BLE,`LOOP2,`R15,`R12};
-  10: oInstruction = {`ADD,`R13,`R13,`R1};
-  11: oInstruction = {`BLE,`LOOP1,`R14,`R13};
-  12: oInstruction = {`LCD,8'b0,`R2,8'b0};
+  9: oInstruction = {`ADD,`R12,`R12,`R1};
+  10: oInstruction = {`BLE,`LOOP2,`R12,`R15};
+  11: oInstruction = {`ADD,`R13,`R13,`R1};
+  12: oInstruction = {`BLE,`LOOP1,`R13,`R14};
+  13: oInstruction = {`LCD,8'b0,`R10,8'b0};
 // LOOPINFINITO:
-  13: oInstruction = {`NOP,24'd0};
-  14: oInstruction = {`BLE,`LOOPINFINITO,`R1,`R0};
+  14: oInstruction = {`BLE,`LOOPINFINITO,`R0,`R1};
   15: oInstruction = {`NOP,24'd0};
   16: oInstruction = {`JMP,`INICIO,16'd0};
 
--- holamundo.asm
+++ holamundo.asm
@@ -2,28 +2,28 @@ NOP,24'd4000
 STO, R0, 16'b0 // cero
 STO, R1, 16'b1 // uno
 
-STO, R13, 16'b0 // contador 1
+STO, R14, 16'd100 // cuenta 1
+STO, R15, 16'd100 // cuenta 2
 
-STO, R14, 14'D1000 // cuenta 1
-STO, R15, 15'D1000 // cuenta 2
+STO, R10, H // H
 
 INICIO:
-STO, R2, H // H
+STO, R13, 16'b0 // contador 1
 
 // Esperar que LCD esté lista
 LOOP1:
   STO, R12, 16'b0 // contador 2
+  NOP,24'd0
   LOOP2:
     ADD, R12, R12, R1
-    BLE, LOOP2, R15, R12
+    BLE, LOOP2, R12, R15
   ADD, R13, R13, R1
-  BLE, LOOP1, R14, R13
+  BLE, LOOP1, R13, R14
 
 // Escribir en LCD
-LCD, 8'b0, R2, 8'b0
+LCD, 8'b0, R10, 8'b0
 
 LOOPINFINITO:
-NOP,24'd0
-BLE, LOOPINFINITO, R1, R0
+BLE, LOOPINFINITO, R0, R1
 NOP,24'd0
 JMP, INICIO, 16'd0

6e66f123275cffadeb8c842a9a4e84382ce1d47a
Emilio Rojas <emilio93@gmail.com>
2018-05-18 00:49:47 -0600

    actualizar codigo de rom con prueba para subrutina

--- Module_ROM.v
+++ Module_ROM.v
@@ -3,9 +3,9 @@
 
 `define INICIO 8'd4
 `define LOOP1 8'd8
-`define LOOP2 8'd9
-`define SUBRUTINA 8'd17
-`define LOOP3 8'd19
+`define LOOP2 8'd10
+`define SUBRUTINA 8'd18
+`define LOOP3 8'd20
 
 module ROM
 (
@@ -21,27 +21,28 @@ begin
   3: oInstruction = {`STO,`R8,16'b11111111};
 // INICIO:
   4: oInstruction = {`STO,`R2,16'b1};
-  5: oInstruction = {`STO,`R3,16'd5};
-  6: oInstruction = {`STO,`R4,16'd10};
-  7: oInstruction = {`STO,`R5,16'd5};
+  5: oInstruction = {`STO,`R3,16'd2};
+  6: oInstruction = {`STO,`R4,16'd1000};
+  7: oInstruction = {`STO,`R5,16'd5000};
 // LOOP1:
   8: oInstruction = {`STO,`R6,16'd0};
+  9: oInstruction = {`MUL,`R2,`R2,`R3};
 // LOOP2:
-  9: oInstruction = {`LED,8'b0,`R5,8'b0};
-  10: oInstruction = {`CALL,`SUBRUTINA,16'b0};
-  11: oInstruction = {`ADD,`R6,`R6,`R1};
-  12: oInstruction = {`BLE,`LOOP2,`R6,`R4};
-  13: oInstruction = {`MUL,`R2,`R2,`R3};
-  14: oInstruction = {`NOP,24'd0};
-  15: oInstruction = {`BLE,`LOOP1,`R2,`R8};
-  16: oInstruction = {`JMP,`INICIO,16'd0};
+  10: oInstruction = {`LED,8'b0,`R6,8'b0};
+  11: oInstruction = {`CALL,`SUBRUTINA,16'b0};
+  12: oInstruction = {`ADD,`R6,`R6,`R1};
+  13: oInstruction = {`BLE,`LOOP2,`R6,`R4};
+  14: oInstruction = {`MUL,`R2,`R2,`R3};
+  15: oInstruction = {`NOP,24'd0};
+  16: oInstruction = {`BLE,`LOOP1,`R2,`R8};
+  17: oInstruction = {`JMP,`INICIO,16'd0};
 // SUBRUTINA:
-  17: oInstruction = {`STO,`R7,16'd0};
-  18: oInstruction = {`NOP,24'd0};
+  18: oInstruction = {`STO,`R7,16'd0};
+  19: oInstruction = {`NOP,24'd0};
 // LOOP3:
-  19: oInstruction = {`ADD,`R7,`R7,`R1};
-  20: oInstruction = {`BLE,`LOOP3,`R7,`R5};
-  21: oInstruction = {`RET,8'b0,8'b0,`RA};
+  20: oInstruction = {`ADD,`R7,`R7,`R1};
+  21: oInstruction = {`BLE,`LOOP3,`R7,`R5};
+  22: oInstruction = {`RET,16'b0,`RA};
 
   default:
     oInstruction = {`LED,24'b10101010}; // NOP
--- subrutina.asm
+++ subrutina.asm
@@ -6,14 +6,15 @@ STO, R8, 16'b11111111 // maximo
 INICIO:
 STO, R2, 16'b1 // valor en leds
 
-STO, R3, 16'd5 // multiplicador
-STO, R4, 16'd10 // cuentas para el ciclo2
-STO, R5, 16'd5 // cuentas para el ciclo3
+STO, R3, 16'd2 // multiplicador
+STO, R4, 16'd1000 // cuentas para el ciclo2
+STO, R5, 16'd5000 // cuentas para el ciclo3
 
 LOOP1:
 STO, R6, 16'd0 // i
+MUL, R2, R2, R3
 LOOP2:
-  LED, 8'b0, R5, 8'b0
+  LED, 8'b0, R6, 8'b0
   CALL, SUBRUTINA, 16'b0
   ADD, R6, R6, R1
   BLE, LOOP2, R6, R4
@@ -28,4 +29,4 @@ SUBRUTINA:
   LOOP3:
     ADD, R7, R7, R1
     BLE, LOOP3, R7, R5
-  RET, 8'b0, 8'b0, RA
+  RET, 16'b0, RA

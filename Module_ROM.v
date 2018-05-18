70bab2cd32b6669ac888bdf9cd901be01284d03a
Emilio Rojas <emilio93@gmail.com>
2018-05-18 02:35:54 -0600

    rom utiliza push y pop

--- Module_ROM.v
+++ Module_ROM.v
@@ -2,10 +2,10 @@
 `include "Defintions.v"
 
 `define INICIO 8'd4
-`define LOOP1 8'd8
-`define LOOP2 8'd10
-`define SUBRUTINA 8'd18
-`define LOOP3 8'd20
+`define LOOP1 8'd12
+`define LOOP2 8'd14
+`define SUBRUTINA 8'd24
+`define LOOP3 8'd27
 
 module ROM
 (
@@ -22,27 +22,35 @@ begin
 // INICIO:
   4: oInstruction = {`STO,`R2,16'b1};
   5: oInstruction = {`STO,`R3,16'd2};
-  6: oInstruction = {`STO,`R4,16'd1000};
-  7: oInstruction = {`STO,`R5,16'd5000};
+  6: oInstruction = {`STO,`R4,16'd65000};
+  7: oInstruction = {`STO,`R5,16'd65000};
+  8: oInstruction = {`PUSH,16'b0,`R3};
+  9: oInstruction = {`PUSH,16'b0,`R4};
+  10: oInstruction = {`PUSH,16'b0,`R5};
+  11: oInstruction = {`STO,`R7,16'd6623};
 // LOOP1:
-  8: oInstruction = {`STO,`R6,16'd0};
-  9: oInstruction = {`MUL,`R2,`R2,`R3};
+  12: oInstruction = {`STO,`R6,16'd0};
+  13: oInstruction = {`MUL,`R2,`R2,`R3};
 // LOOP2:
-  10: oInstruction = {`LED,8'b0,`R6,8'b0};
-  11: oInstruction = {`CALL,`SUBRUTINA,16'b0};
-  12: oInstruction = {`ADD,`R6,`R6,`R1};
-  13: oInstruction = {`BLE,`LOOP2,`R6,`R4};
-  14: oInstruction = {`MUL,`R2,`R2,`R3};
-  15: oInstruction = {`NOP,24'd0};
-  16: oInstruction = {`BLE,`LOOP1,`R2,`R8};
-  17: oInstruction = {`JMP,`INICIO,16'd0};
+  14: oInstruction = {`LED,8'b0,`R6,8'b0};
+  15: oInstruction = {`PUSH,16'b0,`R7};
+  16: oInstruction = {`CALL,`SUBRUTINA,16'b0};
+  17: oInstruction = {`POP,`R7,16'b0};
+  18: oInstruction = {`ADD,`R6,`R6,`R1};
+  19: oInstruction = {`BLE,`LOOP2,`R6,`R4};
+  20: oInstruction = {`MUL,`R2,`R2,`R3};
+  21: oInstruction = {`NOP,24'd0};
+  22: oInstruction = {`BLE,`LOOP1,`R2,`R8};
+  23: oInstruction = {`JMP,`INICIO,16'd0};
 // SUBRUTINA:
-  18: oInstruction = {`STO,`R7,16'd0};
-  19: oInstruction = {`NOP,24'd0};
+  24: oInstruction = {`NOP,24'd0};
+  25: oInstruction = {`STO,`R7,16'd0};
+  26: oInstruction = {`NOP,24'd0};
 // LOOP3:
-  20: oInstruction = {`ADD,`R7,`R7,`R1};
-  21: oInstruction = {`BLE,`LOOP3,`R7,`R5};
-  22: oInstruction = {`RET,16'b0,`RA};
+  27: oInstruction = {`ADD,`R7,`R7,`R1};
+  28: oInstruction = {`BLE,`LOOP3,`R7,`R5};
+  29: oInstruction = {`NOP,24'd0};
+  30: oInstruction = {`RET,16'b0,`RA};
 
   default:
     oInstruction = {`LED,24'b10101010}; // NOP
--- subrutina.asm
+++ subrutina.asm
@@ -7,15 +7,22 @@ INICIO:
 STO, R2, 16'b1 // valor en leds
 
 STO, R3, 16'd2 // multiplicador
-STO, R4, 16'd1000 // cuentas para el ciclo2
-STO, R5, 16'd5000 // cuentas para el ciclo3
+STO, R4, 16'd65000 // cuentas para el ciclo2
+STO, R5, 16'd65000 // cuentas para el ciclo3
+PUSH, 16'b0, R3
+PUSH, 16'b0, R4
+PUSH, 16'b0, R5
+
+STO, R7, 16'd6623 // valor en r7 se mantiene a traves de la subrutina
 
 LOOP1:
 STO, R6, 16'd0 // i
 MUL, R2, R2, R3
 LOOP2:
   LED, 8'b0, R6, 8'b0
+  PUSH, 16'b0, R7
   CALL, SUBRUTINA, 16'b0
+  POP, R7, 16'b0
   ADD, R6, R6, R1
   BLE, LOOP2, R6, R4
 MUL, R2, R2, R3
@@ -24,9 +31,11 @@ BLE, LOOP1, R2, R8
 JMP, INICIO, 16'd0
 
 SUBRUTINA:
+  NOP,24'd0
   STO, R7, 16'd0 // j
   NOP,24'd0
   LOOP3:
     ADD, R7, R7, R1
     BLE, LOOP3, R7, R5
+  NOP,24'd0
   RET, 16'b0, RA

0edb07d03d783ef4bb0ff3d8fa4c07dea4b772b3
Emilio Rojas <emilio93@gmail.com>
2018-05-22 05:46:42 -0600

    mover fuentes asm a folder asm

---
+++ asm/holamundo.asm
@@ -0,0 +1,54 @@
+NOP,24'd4000
+STO, R0, 16'b0 // cero
+STO, R1, 16'b1 // uno
+INICIO:
+CALL, ESCRIBIRLCD, 16'b0
+LOOPINFINITO:
+BLE, LOOPINFINITO, R0, R1
+NOP,24'd0
+JMP, INICIO, 16'd0
+ESCRIBIRLCD:
+NOP, 24'b0
+PUSH, 16'b0, RA
+PUSH, 16'b0, R10
+PUSH, 16'b0, R11
+PUSH, 16'b0, R12
+PUSH, 16'b0, R13
+STO, R10, H // H
+STO, R11, O // O
+STO, R12, L // L
+STO, R13, A // A
+CALL, ESCRIBIR4LETRAS, 16'b0
+STO, R10, ESPACIO //
+STO, R11, M // M
+STO, R12, U // U
+STO, R13, N // N
+CALL, ESCRIBIR4LETRAS, 16'b0
+STO, R10, D // D
+STO, R11, O // O
+STO, R12, ESPACIO //
+STO, R13, ESPACIO //
+CALL, ESCRIBIR4LETRAS, 16'b0
+POP, R13, 16'b0
+POP, R12, 16'b0
+POP, R11, 16'b0
+POP, R10, 16'b0
+POP, RA, 16'b0
+NOP, 24'b0
+RET, 16'b0, RA
+ESCRIBIR4LETRAS:
+NOP, 24'b0
+// Escribir en LCD
+LCD, 8'b0, R10, 8'b0
+// se requiere nop luego
+// de operacion lcd
+// con el fin de esperar
+// que lcd haya escrito
+NOP, 24'b0
+LCD, 8'b0, R11, 8'b0
+NOP, 24'b0
+LCD, 8'b0, R12, 8'b0
+NOP, 24'b0
+LCD, 8'b0, R13, 8'b0
+NOP, 24'b0
+RET, 16'b0, RA

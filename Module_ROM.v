9326955f1e40b6b85a5ba24b5e0e78bd97fdacce
Emilio Rojas <emilio93@gmail.com>
2018-06-15 00:59:34 -0600

    actualizar ROM

--- Module_ROM.v
+++ Module_ROM.v
@@ -1,8 +1,14 @@
 `timescale 1ns / 1ps
 `include "Defintions.v"
 
-`define LOOP1 8'd9
-`define LOOP2 8'd6
+`define INICIO 8'd3
+`define LOOPVERTICAL 8'd10
+`define LOOPHORIZONTAL 8'd11
+`define REGRESOLOOPHORIZONTAL 8'd19
+`define REGRESOLOOPVERTICAL 8'd23
+`define FIN 8'd26
+`define DISPLAY 8'd27
+
 module ROM
 (
 	input  wire[15:0]  		iAddress,
@@ -10,31 +16,58 @@ module ROM
 );	
 always @ ( iAddress )
 begin
-	case (iAddress)
+  case (iAddress)
+  0: oInstruction = {`NOP,24'd4000};
+  1: oInstruction = {`STO,`R0,16'b0};
+  2: oInstruction = {`STO,`R1,16'b1};
+// INICIO:
+  3: oInstruction = {`STO,`R11,16'b0};
+  4: oInstruction = {`STO,`R12,16'b0};
+  5: oInstruction = {`STO,`R13,16'd16};
+  6: oInstruction = {`STO,`R14,16'd12};
+  7: oInstruction = {`STO,`R3,16'd3};
+  8: oInstruction = {`STO,`R4,16'd6};
+  9: oInstruction = {`STO,`R5,16'd9};
+// LOOPVERTICAL:
+  10: oInstruction = {`STO,`R11,16'b0};
+// LOOPHORIZONTAL:
+  11: oInstruction = {`STO,`R10,8'b0,`COLOR_GREEN};
+  12: oInstruction = {`BLE,`REGRESOLOOPHORIZONTAL,`R12,`R3};
+  13: oInstruction = {`STO,`R10,8'b0,`COLOR_RED};
+  14: oInstruction = {`BLE,`REGRESOLOOPHORIZONTAL,`R12,`R4};
+  15: oInstruction = {`STO,`R10,8'b0,`COLOR_MAGENTA};
+  16: oInstruction = {`BLE,`REGRESOLOOPHORIZONTAL,`R12,`R5};
+  17: oInstruction = {`STO,`R10,8'b0,`COLOR_BLUE};
+  18: oInstruction = {`JMP,`REGRESOLOOPHORIZONTAL,16'b0};
+// REGRESOLOOPHORIZONTAL:
+  19: oInstruction = {`BLE,`REGRESOLOOPVERTICAL,`R13,`R11};
+  20: oInstruction = {`CALL,`DISPLAY,16'b0};
+  21: oInstruction = {`ADD,`R11,`R11,`R1};
+  22: oInstruction = {`JMP,`LOOPHORIZONTAL,16'b0};
+// REGRESOLOOPVERTICAL:
+  23: oInstruction = {`BLE,`FIN,`R14,`R12};
+  24: oInstruction = {`ADD,`R12,`R12,`R1};
+  25: oInstruction = {`JMP,`LOOPVERTICAL,16'b0};
+// FIN:
+  26: oInstruction = {`JMP,`FIN,16'b0};
+// DISPLAY:
+  27: oInstruction = {`NOP,24'b0};
+  28: oInstruction = {`PUSH,16'b0,`RA};
+  29: oInstruction = {`PUSH,16'b0,`R2};
+  30: oInstruction = {`NOP,24'b0};
+  31: oInstruction = {`MUL,`R2,`R12,`R13};
+  32: oInstruction = {`NOP,24'b0};
+  33: oInstruction = {`ADD,`R2,`R2,`R11};
+  34: oInstruction = {`NOP,24'b0};
+  35: oInstruction = {`VGA,8'b0,`R10,`R2};
+  36: oInstruction = {`POP,`R2,16'b0};
+  37: oInstruction = {`POP,`RA,16'b0};
+  38: oInstruction = {`NOP,24'b0};
+  39: oInstruction = {`RET,16'b0,`RA};
 
-	0: oInstruction = { `NOP ,24'd4000    };
-	1: oInstruction = { `TEC ,`R7,`R7,8'b0 };
-	2: oInstruction = { `STO ,`R3,16'b000000001001}; 
-	3: oInstruction = { `STO ,`R6,16'b000000000010}; 
-	4: oInstruction = { `STO, `R4,16'd100 };
-	5: oInstruction = { `STO, `R5,16'd0     };  //j
-//LOOP2:
-	6: oInstruction = { `LED ,8'b0,`R7,8'b0 };
-	7: oInstruction = { `STO ,`R1,16'h0     }; 	
-	8: oInstruction = { `STO ,`R2,16'd65 };
-//LOOP1:	
-	9: oInstruction = { `ADD ,`R1,`R1,`R6    }; 
-	10: oInstruction = { `BLE ,`LOOP1,`R1,`R2 }; 
-	
-	11: oInstruction = { `ADD ,`R5,`R5,`R6    };
-	12: oInstruction = { `BLE ,`LOOP2,`R5,`R4 };	
-	13: oInstruction = { `NOP ,24'd4000       }; 
-	//14: oInstruction = { `MUL2 ,`R7,`R7,`R3    };
-	15: oInstruction = { `NOP ,24'd4000       };
-	16: oInstruction = { `JMP ,  8'd1,16'b0   };
-	default:
-		oInstruction = { `LED ,  24'b10101010 };		//NOP
-	endcase	
+  default:
+    oInstruction = {`LED,24'b10101010}; // NOP
+  endcase
 end
-	
+
 endmodule
---
+++ asm/vga.asm
@@ -0,0 +1,98 @@
+NOP,24'd4000
+STO, R0, 16'b0 // cero
+STO, R1, 16'b1 // uno
+
+INICIO:
+
+// Contadores
+STO, R11, 16'b0 // horizontal
+STO, R12, 16'b0 // vertical
+
+// LIMITES
+STO, R13, 16'd16 // horizontal
+STO, R14, 16'd12 // vertical
+
+STO, R3, 16'd3
+STO, R4, 16'd6
+STO, R5, 16'd9
+
+// width = 16, height = 12
+//
+//  gggggggggggggg
+//  rrrrrrrrrrrrrr
+//  mmmmmmmmmmmmmm
+//  bbbbbbbbbbbbbb
+//
+LOOPVERTICAL:
+  STO, R11, 16'b0 // horizontal
+  LOOPHORIZONTAL:
+    STO, R10, 8'b0, COLOR_GREEN
+    BLE, REGRESOLOOPHORIZONTAL, R12, R3
+    STO, R10, 8'b0, COLOR_RED
+    BLE, REGRESOLOOPHORIZONTAL, R12, R4
+    STO, R10, 8'b0, COLOR_MAGENTA
+    BLE, REGRESOLOOPHORIZONTAL, R12, R5
+    STO, R10, 8'b0, COLOR_BLUE
+    JMP, REGRESOLOOPHORIZONTAL, 16'b0
+
+  REGRESOLOOPHORIZONTAL:
+    BLE, REGRESOLOOPVERTICAL, R13, R11
+    CALL, DISPLAY, 16'b0
+    ADD, R11, R11, R1
+    JMP, LOOPHORIZONTAL, 16'b0
+
+REGRESOLOOPVERTICAL:
+  BLE, FIN, R14, R12
+  ADD, R12, R12, R1
+  JMP, LOOPVERTICAL, 16'b0
+
+FIN:
+JMP, FIN, 16'b0
+
+// LE ENTRA R10, R11, R12, R13 y R14
+// PONE EL COLOR INDICADO EN R10
+// EN LA POSICIÓN R11, R12
+// R13 ES EL LIMITE HORIONTAL
+// R14 ES EL LIMITE VERTICAL
+// _______________________
+// |
+// | 0X0 1X0 2X0 ... nX0
+// | 0x1 1x1 2x1 ... nx1
+// |  .   .   . .     .
+// |  .   .   .    .  .
+// |  .   .   .       .
+// | 0xm 1xm 2xm ... nxm
+// |_____________________
+//
+// ______________________
+// |
+// | 0+0n 1+0n 2+0n ... (n-1)+0n
+// | 0+1n 1+1n 2+1n ... (n-1)+1n
+// | 0+2n 1+2n 2+2n ... (n-1)+2n
+// |  .    .    .  .     .
+// |  .    .    .     .  .
+// |  .    .    .        .
+// | 0+mn 1+mn 2+mn  ... (n-1)+nm
+// |_____________________
+//
+// R11=x
+// R12=y
+// R4=limHor
+// R2= R11 + R12 * R4
+DISPLAY:
+  NOP, 24'b0
+  PUSH, 16'b0, RA
+  PUSH, 16'b0, R2 // POSICION EN MEM VGA
+
+  NOP, 24'b0
+  MUL, R2, R12, R13
+  NOP, 24'b0
+  ADD, R2, R2, R11
+
+  NOP, 24'b0
+  VGA, 8'b0, R10, R2
+
+  POP, R2, 16'b0
+  POP, RA, 16'b0
+  NOP, 24'b0
+  RET, 16'b0, RA

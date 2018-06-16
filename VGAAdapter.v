93b0c549e206214f5d406dd194eff1c4915ad984
Emilio Rojas <emilio93@gmail.com>
2018-06-15 22:59:36 -0600

    solucionar errores en vgaadapter

--- VGAAdapter.v
+++ VGAAdapter.v
@@ -6,24 +6,23 @@
 //
 //
 module VGAAdapter # (
-  parameter DATA_WIDTH=3,
   parameter widthOut=640,
   parameter heightOut=480,
   parameter widthIn=16,
   parameter heightIn=12
 ) (
-  input wire [9:0] widthPos,
+  input wire [9:0] widthPos, // solicitud en memoria "grande" 0-widthOut
   input wire [9:0] heightPos,
-  output reg [15:0] widthMin,
-  output reg [12:0] heightMin
+  output reg [3:0] widthMin,
+  output reg [3:0] heightMin
 );
 
 always @ (*) begin
-   widthMin = widthPos < 16 ? 4'd0 :
+   widthMin = widthPos < 40 ? 4'd0 :
                       widthPos >= 40 & widthPos < 80 ? 4'd1:
                       widthPos >= 80 & widthPos < 120 ? 4'd2:
                       widthPos >= 120 & widthPos < 160 ? 4'd3:
-                      widthPos >= 160 & widthPos < 160 ? 4'd4:
+                      widthPos >= 160 & widthPos < 200 ? 4'd4:
                       widthPos >= 200 & widthPos < 240 ? 4'd5:
                       widthPos >= 240 & widthPos < 280 ? 4'd6:
                       widthPos >= 280 & widthPos < 320 ? 4'd7:
@@ -36,32 +35,18 @@ always @ (*) begin
                       widthPos >= 560 & widthPos < 600 ? 4'd14:
                       widthPos >= 600 & widthPos < 640 ? 5'd15: 0;
 
-  heightMin = heightPos < 16 ? 4'd0 :
-                      heightPos >= 40  & heightPos < 80 ? 4'd1:
-                      heightPos >= 80  & heightPos < 120 ? 4'd2:
-                      heightPos >= 120 & heightPos < 160 ? 4'd3:
-                      heightPos >= 160 & heightPos < 160 ? 4'd4:
-                      heightPos >= 200 & heightPos < 240 ? 4'd5:
-                      heightPos >= 240 & heightPos < 280 ? 4'd6:
-                      heightPos >= 280 & heightPos < 320 ? 4'd7:
-                      heightPos >= 320 & heightPos < 360 ? 4'd8:
-                      heightPos >= 360 & heightPos < 400 ? 4'd9:
-                      heightPos >= 400 & heightPos < 440 ? 4'd10:
-                      heightPos >= 440 & heightPos < 480 ? 4'd11: 0;
+  heightMin = heightPos < 40 ? 4'd0 :
+                      heightPos < 80 ? 4'd1:
+                      heightPos < 120 ? 4'd2:
+                      heightPos < 160 ? 4'd3:
+                      heightPos < 200 ? 4'd4:
+                      heightPos < 240 ? 4'd5:
+                      heightPos < 280 ? 4'd6:
+                      heightPos < 320 ? 4'd7:
+                      heightPos < 360 ? 4'd8:
+                      heightPos < 400 ? 4'd9:
+                      heightPos < 440 ? 4'd10:
+                      heightPos < 480 ? 4'd11: 0;
 
 end
-//  genvar i, j;
-//  generate
-//    for (i=0; i<countH; i=i+1) begin : for_no_i
-//		  if (widthPos >= i*countH & widthPos < (i+1)*countH) begin
-//		    widthMin = i;
-//		  end
-//	 end
-//	for (j=0; j<countV; j=j+1) begin : for_no_j
-//		  if (heightPos >= $j*countV & heightPos < ($j+1)*countV) begin
-//          heightMin = $j;
-//		end
-//	end
-//  endgenerate
-
 endmodule

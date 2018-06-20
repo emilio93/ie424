c03f01d128c01840cffe4b7525337a1275016470
Unknown <ema2159@gmail.com>
2018-06-20 10:46:15 -0600

    Creación de día y noche

--- Module_ROM.v
+++ Module_ROM.v
@@ -1,69 +1,136 @@
 `timescale 1ns / 1ps
 `include "Defintions.v"
- 
+
 `define INICIO 8'd3
-`define LOOPVERTICAL 8'd10
-`define LOOPHORIZONTAL 8'd11
-`define REGRESOLOOPHORIZONTAL 8'd19
-`define REGRESOLOOPVERTICAL 8'd23
-`define FIN 8'd26
-`define DISPLAY 8'd27
- 
+`define GAME 8'd10
+`define WAIT1 8'd13
+`define WAIT2 8'd14
+`define CONTINUE 8'd18
+`define FIN 8'd19
+`define DIA 8'd20
+`define LOOPVERTICAL 8'd23
+`define LOOPHORIZONTAL 8'd24
+`define REGRESOLOOPHORIZONTAL 8'd28
+`define REGRESOLOOPVERTICAL 8'd32
+`define SOL 8'd35
+`define NOCHE 8'd43
+`define LOOPVERTICAL2 8'd46
+`define LOOPHORIZONTAL2 8'd47
+`define REGRESOLOOPHORIZONTAL2 8'd51
+`define REGRESOLOOPVERTICAL2 8'd55
+`define LUNA 8'd58
+`define DISPLAY 8'd66
+
 module ROM
 (
-  input  wire[15:0]      iAddress,
-  output reg [27:0]     oInstruction
-);  
-always @ ( iAddress )
+  input  wire[15:0] iAddress,
+  output reg [27:0] oInstruction
+);
+always @ (iAddress)
 begin
   case (iAddress)
   0: oInstruction = {`NOP,24'd4000};
   1: oInstruction = {`STO,`R0,16'b0};
   2: oInstruction = {`STO,`R1,16'b1};
 // INICIO:
-  3: oInstruction = {`STO,`R11,16'b0}; //contador x
-  4: oInstruction = {`STO,`R12,16'b0}; //contador y
+  3: oInstruction = {`STO,`R11,16'b0};
+  4: oInstruction = {`STO,`R12,16'b0};
   5: oInstruction = {`STO,`R13,16'd16};
   6: oInstruction = {`STO,`R14,16'd12};
-  7: oInstruction = {`STO,`R3,16'd9};
+  7: oInstruction = {`STO,`R3,16'd5};
   8: oInstruction = {`STO,`R4,16'd5};
   9: oInstruction = {`STO,`R5,16'd8};
-// LOOPVERTICAL:
-  10: oInstruction = {`STO,`R11,16'b0};
-// LOOPHORIZONTAL:
-  11: oInstruction = {`STO,`R10,8'b0,`COLOR_CYAN};
-  12: oInstruction = {`BLE,`REGRESOLOOPHORIZONTAL,`R12,`R3};
-  17: oInstruction = {`STO,`R10,8'b0,`COLOR_GREEN};
-  18: oInstruction = {`JMP,`REGRESOLOOPHORIZONTAL,16'b0};
-// REGRESOLOOPHORIZONTAL:
-  19: oInstruction = {`BLE,`REGRESOLOOPVERTICAL,`R13,`R11};
-  20: oInstruction = {`CALL,`DISPLAY,16'b0};
-  21: oInstruction = {`ADD,`R11,`R11,`R1};
-  22: oInstruction = {`JMP,`LOOPHORIZONTAL,16'b0};
-// REGRESOLOOPVERTICAL:
-  23: oInstruction = {`BLE,`FIN,`R14,`R12};
-  24: oInstruction = {`ADD,`R12,`R12,`R1};
-  25: oInstruction = {`JMP,`LOOPVERTICAL,16'b0};
+// GAME:
+  10: oInstruction = {`CALL,`DIA,16'b0};
+  11: oInstruction = {`STO,`R3,16'b0};
+  12: oInstruction = {`STO,`R4,16'd25000};
+// WAIT1:
+  13: oInstruction = {`STO,`R2,1'b0};
+// WAIT2:
+  14: oInstruction = {`ADD,`R2,`R2,`R1};
+  15: oInstruction = {`BLE,`WAIT2,`R2,`R4};
+  16: oInstruction = {`ADD,`R3,`R3,`R1};
+  17: oInstruction = {`BLE,`WAIT1,`R2,`R4};
+// CONTINUE:
+  18: oInstruction = {`CALL,`NOCHE,15'b0};
 // FIN:
-  26: oInstruction = {`JMP,`FIN,16'b0};
+  19: oInstruction = {`JMP,`FIN,16'b0};
+// DIA:
+  20: oInstruction = {`NOP,24'd0};
+  21: oInstruction = {`PUSH,16'b0,`RA};
+  22: oInstruction = {`STO,`R12,16'b0};
+// LOOPVERTICAL:
+  23: oInstruction = {`STO,`R11,16'b0};
+// LOOPHORIZONTAL:
+  24: oInstruction = {`STO,`R10,8'b0,`COLOR_CYAN};
+  25: oInstruction = {`BLE,`REGRESOLOOPHORIZONTAL,`R12,`R3};
+  26: oInstruction = {`STO,`R10,8'b0,`COLOR_GREEN};
+  27: oInstruction = {`JMP,`REGRESOLOOPHORIZONTAL,16'b0};
+// REGRESOLOOPHORIZONTAL:
+  28: oInstruction = {`BLE,`REGRESOLOOPVERTICAL,`R13,`R11};
+  29: oInstruction = {`CALL,`DISPLAY,16'b0};
+  30: oInstruction = {`ADD,`R11,`R11,`R1};
+  31: oInstruction = {`JMP,`LOOPHORIZONTAL,16'b0};
+// REGRESOLOOPVERTICAL:
+  32: oInstruction = {`BLE,`SOL,`R14,`R12};
+  33: oInstruction = {`ADD,`R12,`R12,`R1};
+  34: oInstruction = {`JMP,`LOOPVERTICAL,16'b0};
+// SOL:
+  35: oInstruction = {`NOP,24'd0};
+  36: oInstruction = {`STO,`R11,16'd14};
+  37: oInstruction = {`STO,`R12,16'b1};
+  38: oInstruction = {`STO,`R10,8'b0,`COLOR_YELLOW};
+  39: oInstruction = {`CALL,`DISPLAY,16'b0};
+  40: oInstruction = {`POP,`RA,16'b0};
+  41: oInstruction = {`NOP,24'd0};
+  42: oInstruction = {`RET,16'b0,`RA};
+// NOCHE:
+  43: oInstruction = {`NOP,24'd0};
+  44: oInstruction = {`PUSH,16'b0,`RA};
+  45: oInstruction = {`STO,`R12,16'b0};
+// LOOPVERTICAL2:
+  46: oInstruction = {`STO,`R11,16'b0};
+// LOOPHORIZONTAL2:
+  47: oInstruction = {`STO,`R10,8'b0,`COLOR_BLACK};
+  48: oInstruction = {`BLE,`REGRESOLOOPHORIZONTAL2,`R12,`R3};
+  49: oInstruction = {`STO,`R10,8'b0,`COLOR_GREEN};
+  50: oInstruction = {`JMP,`REGRESOLOOPHORIZONTAL2,16'b0};
+// REGRESOLOOPHORIZONTAL2:
+  51: oInstruction = {`BLE,`REGRESOLOOPVERTICAL2,`R13,`R11};
+  52: oInstruction = {`CALL,`DISPLAY,16'b0};
+  53: oInstruction = {`ADD,`R11,`R11,`R1};
+  54: oInstruction = {`JMP,`LOOPHORIZONTAL2,16'b0};
+// REGRESOLOOPVERTICAL2:
+  55: oInstruction = {`BLE,`LUNA,`R14,`R12};
+  56: oInstruction = {`ADD,`R12,`R12,`R1};
+  57: oInstruction = {`JMP,`LOOPVERTICAL2,16'b0};
+// LUNA:
+  58: oInstruction = {`NOP,24'd0};
+  59: oInstruction = {`STO,`R11,16'd14};
+  60: oInstruction = {`STO,`R12,16'b1};
+  61: oInstruction = {`STO,`R10,8'b0,`COLOR_WHITE};
+  62: oInstruction = {`CALL,`DISPLAY,16'b0};
+  63: oInstruction = {`POP,`RA,16'b0};
+  64: oInstruction = {`NOP,24'd0};
+  65: oInstruction = {`RET,16'b0,`RA};
 // DISPLAY:
-  27: oInstruction = {`NOP,24'b0};
-  28: oInstruction = {`PUSH,16'b0,`RA};
-  29: oInstruction = {`PUSH,16'b0,`R2};
-  30: oInstruction = {`NOP,24'b0};
-  31: oInstruction = {`MUL,`R2,`R12,`R13};
-  32: oInstruction = {`NOP,24'b0};
-  33: oInstruction = {`ADD,`R2,`R2,`R11};
-  34: oInstruction = {`NOP,24'b0};
-  35: oInstruction = {`VGA,8'b0,`R10,`R2};
-  36: oInstruction = {`POP,`R2,16'b0};
-  37: oInstruction = {`POP,`RA,16'b0};
-  38: oInstruction = {`NOP,24'b0};
-  39: oInstruction = {`RET,16'b0,`RA};
- 
+  66: oInstruction = {`NOP,24'b0};
+  67: oInstruction = {`PUSH,16'b0,`RA};
+  68: oInstruction = {`PUSH,16'b0,`R2};
+  69: oInstruction = {`NOP,24'b0};
+  70: oInstruction = {`MUL,`R2,`R12,`R13};
+  71: oInstruction = {`NOP,24'b0};
+  72: oInstruction = {`ADD,`R2,`R2,`R11};
+  73: oInstruction = {`NOP,24'b0};
+  74: oInstruction = {`VGA,8'b0,`R10,`R2};
+  75: oInstruction = {`POP,`R2,16'b0};
+  76: oInstruction = {`POP,`RA,16'b0};
+  77: oInstruction = {`NOP,24'b0};
+  78: oInstruction = {`RET,16'b0,`RA};
+
   default:
     oInstruction = {`LED,24'b10101010}; // NOP
   endcase
 end
- 
+
 endmodule
---
+++ translate.js
@@ -0,0 +1,192 @@
+// Copyright 2018 Emilio Rojas
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+const fs = require('fs');
+const readline = require('readline');
+
+// Use:
+// node ./translate.js mycode.asm
+
+// Gets input filename from the command
+let getInputFile = (args, v=false) => {
+  // Exit if no input file is provided
+  if (typeof args[2] === 'undefined') {
+    console.log('input file required');
+    process.exit();
+  }
+  let asmFilename = args[2];
+  // Print info if v=true
+  if (v) console.log(`${asmFilename} set as input file`);
+  return asmFilename;
+};
+
+// Gets output filename from the command
+// Defaults to input filename with .v extension
+let getOutputFile = (args, v=false) => {
+  let vlFilename = args[3];
+  if (typeof args[3] === 'undefined') {
+    if (v) console.log('no output file specified');
+    vlFilename = getInputFile(args).replace('.asm','.v');
+  }
+
+  if (v) console.log(`${vlFilename} set as output file`);
+  return vlFilename;
+};
+
+// Creates the verilog module code.
+//
+let generateModule = (lines,dataWidth=16,instructionWidth=28,includeFile="Defintions.v", moduleName="ROM", iAddress="iAddress", oInstruction="oInstruction") => {
+  let tags = '';
+  let instructions = '';
+
+  lines.forEach((val, idx, arr) => {
+    if (val.type==='tag')
+      tags = `${tags}\`define ${val.tag} 8'd${val.tagIp}\n`;
+  });
+
+  lines.forEach((val, idx, arr) => {
+    if (val.type==='tag') {
+      instructions = `${instructions}// ${val.tag}:\n`;
+    }
+    else if (val.type==='op') {
+      let currLine=[];
+      val.opParams.forEach((val,idx,arr) => {
+        if (idx===0||!val[0].match(/[A-Z]/i)) {
+          currLine.push(val);
+        }
+        else {
+          currLine.push(`\`${val}`)
+        }
+      });
+      instructions = `${instructions}  ${val.opIp}: ${oInstruction} = {\`${currLine.join(',')}};\n`;
+    }
+  });
+
+return `\`timescale 1ns / 1ps
+\`include "${includeFile}"
+
+${tags}
+module ${moduleName}
+(
+  input  wire[${dataWidth-1}:0] ${iAddress},
+  output reg [${instructionWidth-1}:0] ${oInstruction}
+);
+always @ (${iAddress})
+begin
+  case (${iAddress})
+${instructions}
+  default:
+    ${oInstruction} = {\`LED,24'b10101010}; // NOP
+  endcase
+end
+
+endmodule
+`;
+}
+
+// array
+// 0 : process path (node)
+// 1 : script name (translate.js)
+// 2 : asm file (mycode.js)
+let arguments = process.argv;
+
+let asmFilename = getInputFile(arguments);
+
+let vlFilename = getOutputFile(arguments);
+
+// output lines of code represented as
+// {
+//   type     : 'tag' | 'op', // tags or operations are supported
+//
+//   // Required if type is tag
+//   tag      : String,        // name of the loop, first char must be alfabetic,
+//                             // tags can have alfanumeric chars and underscore(_).
+//   tagIp    : int,           // instruction number to which the tag points to.
+//
+//   opType   : 2 | 3 | 4,    // depends on quantity of strings separated by a comma (,)
+//   opParams : [],           // Instruction_id, param_0, [[param_1], param_2] //[comments]
+let output = [];
+
+
+// Regexp's give information about a given line
+let regexpTag = /\s*([A-Z]+[A-Z0-9_]+)\s*:\s*/gi;
+let regexpOp1 = /^\s*([A-Z0-9]+)\s*,\s*([A-Z0-9'_]+)\s*$/img;
+let regexpOp2 = /^\s*([A-Z0-9]+)\s*,\s*([A-Z0-9'_]+)\s*,\s*([A-Z0-9'_]+)\s*(|\/\/.*)*$/gmi;
+let regexpOp3 = /^\s*([A-Z0-9]+)\s*,\s*([A-Z0-9'_]+)\s*,\s*([A-Z0-9'_]+)\s*,\s*([A-Z0-9'_]+)\s*(|\/\/.*)*$/gmi;
+
+// Line reader to read the asm file
+let asmStream = fs.createReadStream(asmFilename);
+let lineReader = readline.createInterface({
+  input: asmStream
+});
+
+i=0;
+ip=0;
+lineReader.on('line', function (line) {
+  i++;
+  if(line.match(regexpTag)) {
+    let match = regexpTag.exec(line);
+    output.push({
+      'type':'tag',
+      'tag':match[1],
+      'tagIp':ip,
+      'opType': '',
+      'opParams': '',
+      'opIp': ''
+    });
+  }
+
+  else if (line.match(regexpOp1)) {
+    let match = regexpOp1.exec(line);
+    output.push({
+      'type':'op',
+      'tag':'',
+      'tagIp':'',
+      'opType': 1,
+      'opParams': [match[1],match[2]],
+      'opIp': ip++
+    });
+  }
+
+  else if (line.match(regexpOp2)) {
+    let match = regexpOp2.exec(line);
+    output.push({
+      'type':'op',
+      'tag':'',
+      'tagIp':'',
+      'opType':2,
+      'opParams':[match[1],match[2],match[3]],
+      'opIp': ip++
+    });
+  }
+
+  else if (line.match(regexpOp3)) {
+    let match = regexpOp3.exec(line);
+    output.push({
+      'type':'op',
+      'tag':'',
+      'tagIp':'',
+      'opType':3,
+      'opParams':[match[1],match[2],match[3],match[4]],
+      'opIp': ip++
+    });
+  }
+
+  else console.log(`${i}: IGNORED LINE\t${line}`);
+});
+
+// Write verilog file when done reading
+lineReader.on('close', () => {
+  fs.writeFile(vlFilename, generateModule(output), function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log(`File ${vlFilename} was written.`);
+    console.log(generateModule(output));
+  });
+});

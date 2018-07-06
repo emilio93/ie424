390a45c991af0e9ee9377cf321d5782eab38036f
Emilio Rojas <emilio93@gmail.com>
2018-07-06 00:38:32 -0600

    correcciones en main.asm

--- asm/main.asm
+++ asm/main.asm
@@ -3,13 +3,16 @@ STO, R0, 16'b0 // cero
 STO, R1, 16'b1 // uno
 
 INICIO:
+
+
+JUEGO:
   NOP, 24'b0
-  TEC R22, R22, 8'b0
+  TEC, R22, R22, 8'b0
   NOP, 24'b0
-  CALL, MOVIMIENTO
-  CALL, FONDO
-  CALL, PINTAR_JUGADOR
+  CALL, MOVIMIENTO, 16'b0
+  CALL, FONDO, 16'b0
+  CALL, PINTAR_JUGADOR, 16'b0
 
   //puede ser que sea necesario un loop largo aqui
 
-  JMP, INICIO
+  JMP, JUEGO, 16'b0

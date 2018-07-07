fe937e35996d7d73d84a2e4492e29282c733f1e6
Emilio Rojas <emilio93@gmail.com>
2018-07-07 17:15:18 -0600

    correcto pintado del dia y noche
    corrige error en dia, se mostraba cortado el zacate

--- asm/routines/PONER_CIELO_DIA.asm
+++ asm/routines/PONER_CIELO_DIA.asm
@@ -3,25 +3,19 @@ PONER_CIELO_DIA:
   PUSH, 16'b0, RA
   PUSH, 16'b0, R16
   PUSH, 16'b0, R15
-  PUSH, 16'b0, R12
-  PUSH, 16'b0, R11
 
   STO, R10, 8'b0, COLOR_CYAN
 
   # limites rectangulo
   STO, R11, 16'd0
   STO, R12, 16'd0
+  STO, R13, 16'd40
   STO, R15, 16'd40
   STO, R16, 16'd18
-
-  STO, R13, 16'd40
-  STO, R14, 16'd30
 
   # dibujar rectangulo
   CALL, PINTAR_RECTANGULO, 16'b0
 
-  POP, R11, 16'b0
-  POP, R12, 16'b0
   POP, R15, 16'b0
   POP, R16, 16'b0
   POP, RA, 16'b0
--- asm/routines/PONER_CIELO_NOCHE.asm
+++ asm/routines/PONER_CIELO_NOCHE.asm
@@ -5,11 +5,11 @@ PONER_CIELO_NOCHE:
   PUSH, 16'b0, R15
 
   STO, R10, 8'b0, COLOR_BLACK
-  STO, R12, 16'b0
 
   # limites rectangulo
   STO, R11, 16'd0
   STO, R12, 16'd0
+  STO, R13, 16'd40
   STO, R15, 16'd40
   STO, R16, 16'd18
 

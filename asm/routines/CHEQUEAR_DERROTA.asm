a97969deb80f633d7b1b47fb36a45a1743359b53
Emilio Rojas <emilio93@gmail.com>
2018-07-11 10:31:49 -0600

    Añadida trompita

--- asm/routines/CHEQUEAR_DERROTA.asm
+++ asm/routines/CHEQUEAR_DERROTA.asm
@@ -1,6 +1,6 @@
 // Esta es la lógica del juego
 //
-//Se chequea si el jugador ha perdido el juego 
+//Se chequea si el jugador ha perdido el juego
 //verificando si hay colisión
 CHEQUEAR_DERROTA:
   NOP, 30'b0
@@ -8,12 +8,15 @@ CHEQUEAR_DERROTA:
   STO, T1, 20'd19
   STO, T2, 20'd24
   STO, T3, 20'd6
-  
-  
+	STO, T4, 20'd7
+
+
 	BEQ, PLAYER_UP, R21, T1
 	BEQ, PLAYER_DOWN, R21, T2
-	
+
 	PLAYER_UP:
+		BEQ, DERROTADO, R24, T4
+		NOP, 30'b0
 		BEQ, DERROTADO, R24, T3
 		SUB, T3, T3, R1
 		NOP, 30'b0
@@ -23,7 +26,7 @@ CHEQUEAR_DERROTA:
 		BEQ, DERROTADO, R24, T3
 		NOP, 30'b0
 		JMP, NO_DERROTA, 20'd0
-	
+
 	PLAYER_DOWN:
 		BEQ, DERROTADO, R22, T3
 		SUB, T3, T3, R1
@@ -39,7 +42,7 @@ CHEQUEAR_DERROTA:
 		POP, RA, 20'b0
 		NOP, 30'b0
 		JMP, GAME_LOST, 20'd0
-		
+
 	NO_DERROTA:
     POP, RA, 20'b0
     NOP, 30'b0
--- asm/routines/PINTAR_JUGADOR.asm
+++ asm/routines/PINTAR_JUGADOR.asm
@@ -5,6 +5,17 @@ PINTAR_JUGADOR:
   PUSH, 20'b0, R19
 
   STO, R10, 10'b0, COLOR_MAGENTA
+  STO, R18, 20'd2 //ancho del jugador = R18+1
+  STO, R19, 20'd1 //alto del jugador = R19+1
+
+  # limites rectangulo
+  ADD, R11, R20, R0
+  ADD, R12, R21, R0
+  ADD, R15, R20, R18
+  ADD, R16, R21, R19
+
+  # dibujar rectangulo
+  CALL, PINTAR_RECTANGULO, 20'b0
 
   STO, R18, 20'd1 //ancho del jugador = R18+1
   STO, R19, 20'd4 //alto del jugador = R19+1

46d0e93a96c1bea29fa4ecdb6138edd060a0d0a9
Emilio Rojas <emilio93@gmail.com>
2018-07-11 08:58:52 -0600

    Mejoradas nubes

--- asm/routines/PINTAR_NUBES.asm
+++ asm/routines/PINTAR_NUBES.asm
@@ -1,13 +1,13 @@
 PINTAR_NUBES:
   NOP, 30'b0
   PUSH, 20'b0, RA
-
+  # nube uno
   STO, R10, 10'b0, COLOR_WHITE
   STO, R18, 20'd5 //ancho de la nube
-  STO, R19, 20'd2 //alto de la nube
+  STO, R19, 20'd1 //alto de la nube
 	STO, T3, 20'd3
 	STO, T4, 20'd3
-	
+
   # limites rectangulo
   ADD, R11, T3, R0
   ADD, R12, T4, R0
@@ -16,12 +16,27 @@ PINTAR_NUBES:
 
   # dibujar rectangulo
   CALL, PINTAR_RECTANGULO, 20'b0
-  
+
+  STO, R18, 20'd3 //ancho de la nube
+  STO, R19, 20'd3 //alto de la nube
+	STO, T3, 20'd4
+	STO, T4, 20'd2
+
+  # limites rectangulo
+  ADD, R11, T3, R0
+  ADD, R12, T4, R0
+  ADD, R15, T3, R18
+  ADD, R16, T4, R19
+
+  # dibujar rectangulo
+  CALL, PINTAR_RECTANGULO, 20'b0
+
+  # nube 2
   STO, T3, 20'd10
 	STO, T4, 20'd9
 	STO, R18, 20'd4 //ancho de la nube
-  STO, R19, 20'd2 //alto de la nube
-	
+  STO, R19, 20'd1 //alto de la nube
+
   # limites rectangulo
   ADD, R11, T3, R0
   ADD, R12, T4, R0
@@ -30,12 +45,41 @@ PINTAR_NUBES:
 
   # dibujar rectangulo
   CALL, PINTAR_RECTANGULO, 20'b0
-  
+
+  STO, T3, 20'd11
+	STO, T4, 20'd8
+	STO, R18, 20'd2 //ancho de la nube
+  STO, R19, 20'd3 //alto de la nube
+
+  # limites rectangulo
+  ADD, R11, T3, R0
+  ADD, R12, T4, R0
+  ADD, R15, T3, R18
+  ADD, R16, T4, R19
+
+  # dibujar rectangulo
+  CALL, PINTAR_RECTANGULO, 20'b0
+
+  # nube 3
   STO, T3, 20'd27
 	STO, T4, 20'd8
 	STO, R18, 20'd4 //ancho de la nube
-  STO, R19, 20'd2 //alto de la nube
-	
+  STO, R19, 20'd1 //alto de la nube
+
+  # limites rectangulo
+  ADD, R11, T3, R0
+  ADD, R12, T4, R0
+  ADD, R15, T3, R18
+  ADD, R16, T4, R19
+
+  # dibujar rectangulo
+  CALL, PINTAR_RECTANGULO, 20'b0
+
+  STO, T3, 20'd28
+	STO, T4, 20'd7
+	STO, R18, 20'd2 //ancho de la nube
+  STO, R19, 20'd3 //alto de la nube
+
   # limites rectangulo
   ADD, R11, T3, R0
   ADD, R12, T4, R0
